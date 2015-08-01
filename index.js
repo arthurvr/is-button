87a590e1b0a4be4eedc01dbabe3f919e43b14f19
drkraken <kuchuklehjs@gmail.com>
2015-08-02 00:45:30 +0300

    small improvement

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use strict';
+
 module.exports = function (element) {
 	if (!element || element.nodeType !== 1) {
 		throw new TypeError('Expected a DOM node to check');
 	}
 
+	var regNames = /(btn|button|^(b)\b|\b(but)\b)/gi;
+	var regWhitespaces = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
 	var tagName = element.tagName.toLowerCase();
 
-	return tagName === 'button' || (tagName === 'input' && element.type === 'button');
+	return tagName === 'button' || (tagName === 'input' && element.type === 'button') || (tagName === 'a' && element.className.replace(regWhitespaces, '').match(regNames));
 };
